2b3ccd8ea5e845dc220485e9782f42464cac52bd
Rohit Jain <rohitjain3241@gmail.com>
2018-02-28 10:13:27 +0530

    Flatten tree Solution

---
+++ Tree/Medium/Flatten_Binary_Tree_to_Linked_List.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    //This keeps the previous pointer
+    TreeNode* prev;
+    
+    //This is basically the preorder traversal
+    void f(TreeNode* root){
+        //If reached NULL pointer
+        if(!root) 
+            return;
+        
+        //Moving the prev pointer to the left
+        prev = root;
+        
+        //As left is already linked we don't need to do anything
+        f(root->left);
+        
+        //previous pointer(i.e. leftmost node uptil now) is linked to this right child
+        if(root->right)
+            prev->left = root->right;
+        
+        //Similarly doing this for right child too
+        f(root->right);
+    }
+    
+public:
+    void flatten(TreeNode* root) {
+        f(root);
+        TreeNode* temp = root;
+        
+        //This part is just for converting the left sided tree to right sided tree
+        while(temp){
+            temp->right = temp->left;
+            temp->left = NULL;
+            temp = temp->right;
+        }
+    }
+};
